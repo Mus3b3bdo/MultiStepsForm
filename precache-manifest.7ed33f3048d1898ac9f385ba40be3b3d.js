217fbde0bd2f0ff8f4f2df53ca226f975971b815
Mus3b3bdo <musabashra1995@gmail.com>
2023-12-23 21:39:57 +0200

    Updates

---
+++ precache-manifest.7ed33f3048d1898ac9f385ba40be3b3d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb0003ef4adc4377924f",
+    "url": "/MultiStepsForm/static/css/main.20b800a4.chunk.css"
+  },
+  {
+    "revision": "cb0003ef4adc4377924f",
+    "url": "/MultiStepsForm/static/js/main.cb0003ef.chunk.js"
+  },
+  {
+    "revision": "50c35ef5f47870a5d5e3",
+    "url": "/MultiStepsForm/static/js/1.50c35ef5.chunk.js"
+  },
+  {
+    "revision": "2d1d445d6a264e4f322a",
+    "url": "/MultiStepsForm/static/js/runtime~main.2d1d445d.js"
+  },
+  {
+    "revision": "e999314c3ef64cc95cebc2427a87414a",
+    "url": "/MultiStepsForm/static/media/icon-arcade.e999314c.svg"
+  },
+  {
+    "revision": "4f6f45aad5b6d66e7e23b5cff237f718",
+    "url": "/MultiStepsForm/static/media/icon-advanced.4f6f45aa.svg"
+  },
+  {
+    "revision": "9321837a44718e56638aaf4410db2470",
+    "url": "/MultiStepsForm/static/media/icon-pro.9321837a.svg"
+  },
+  {
+    "revision": "896a60219f6157eab096825a0c9348a8",
+    "url": "/MultiStepsForm/static/media/Ubuntu-Bold.896a6021.ttf"
+  },
+  {
+    "revision": "84ea7c5c9d2fa40c070ccb901046117d",
+    "url": "/MultiStepsForm/static/media/Ubuntu-Regular.84ea7c5c.ttf"
+  },
+  {
+    "revision": "d3c3b35e6d478ed149f02fad880dd359",
+    "url": "/MultiStepsForm/static/media/Ubuntu-Medium.d3c3b35e.ttf"
+  },
+  {
+    "revision": "a869a489582341f34d24f54aecdaabab",
+    "url": "/MultiStepsForm/static/media/bg-sidebar-desktop.a869a489.svg"
+  },
+  {
+    "revision": "a80e58e4b8751e0621e2461b289747d4",
+    "url": "/MultiStepsForm/static/media/icon-thank-you.a80e58e4.svg"
+  },
+  {
+    "revision": "4558f8b04d8f1c46dc4b855d62c80eae",
+    "url": "/MultiStepsForm/static/media/bg-sidebar-mobile.4558f8b0.svg"
+  },
+  {
+    "revision": "1e38fad428ade66e072dd59ccdb17d68",
+    "url": "/MultiStepsForm/index.html"
+  }
+];
